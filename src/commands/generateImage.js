9c32c7d046a080b8a439363cfcc263248255c417
Valentin Zwerschke <v.zwerschke@mail.de>
2022-06-15 21:26:31 +0200

    Add gen image

--- src/commands/generateImage.js
+++ src/commands/generateImage.js
@@ -1,13 +1,44 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
+const {
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+  MessageAttachment,
+} = require("discord.js");
+const axios = require("axios");
+const jsonUtils = require("../utils/jsonUtils");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("ping").setDescription("Pong?"),
+  data: new SlashCommandBuilder()
+    .setName("generate_image")
+    .setDescription("Generate an image via description")
+    .addStringOption((option) =>
+      option
+        .setName("description")
+        .setDescription("Description of the image")
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    return await interaction.reply({
-      content:
-        "Pong! Replied in `" + Math.round(interaction.client.ws.ping) + "ms`.",
-      ephemeral: true,
+    jsonUtils.readJson((data) => {
+      const serverUrl = data[interaction.guildId];
+      if (serverUrl == undefined)
+        return interaction.reply(
+          "Set colab server URL first with /set_colab_server"
+        );
+      interaction.reply("Sending request (this could take a while)...");
+
+      axios
+        .post(serverUrl + "/dalle", { text: "test", num_images: 1 })
+        .then((res) => {
+          const str = res.data;
+          const sfbuff = new Buffer.from(str, "base64");
+          console.log(sfbuff);
+          const sfattach = new MessageAttachment(sfbuff, "output.png");
+          interaction.channel.send(sfattach);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     });
   },
 };
